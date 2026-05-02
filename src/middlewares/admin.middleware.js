508d19370ed7e3e33704b3350b6faf344ba54306
Hkaar <HkaarDev@gmail.com>
2026-05-02 11:46:17 +0800

    feat: added auth middleware and role access authorization

---
+++ src/middlewares/admin.middleware.js
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+
+export const authorizeAdmin = (req, res, next) => {
+  // Memeriksa apakah user adalah admin
+  // dengan memeriksa properti role pada objek user yang sudah di-decode dari token JWT
+  console.log(req.user);
+  if (!req.user || req.user.role !== "ADMIN") {
+    return res.status(403).json({ error: "Access denied. Admins only." });
+  }
+
+  next();
+};
